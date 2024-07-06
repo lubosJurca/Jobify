8f3e965911242f3ba82f07970dfa7d233cf8d9e7
Lubos Jurca <lubos.jurca@gmail.com>
2024-07-06 22:29:08 +0200

    cloudinary setup changed

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
 import Job from '../models/JobModel.js';
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs';
+import { formatImage } from '../middleware/multerMiddleware.js';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -20,10 +20,10 @@ export const updateUser = async (req, res) => {
   delete newUser.password;
 
   if (req.file) {
+    const file = formatImage(req.file);
+
     // upload image to cloudinary
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    // delete old image from local
-    await fs.unlink(req.file.path);
+    const response = await cloudinary.v2.uploader.upload(file);
 
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
